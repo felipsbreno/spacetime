d761e246eccf07142c0c9539967314e92be006ca
felipsbreno <brendo.filipe2050@gmail.com>
2023-06-03 19:10:48 -0300

    install same dependeces and others changes

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import { fastify } from 'fastify';
+
+const portServer: number = 3333;
+const app = fastify();
+
+app.get('/hello', () => {
+  return 'HTTP/1.1 200 OK';
+});
+
+app
+  .listen({
+    port: portServer,
+  })
+  .then(() => {
+    console.log(
+      '🚀 HTTP server running on port ' + `http://localhost:${portServer}`
+    );
+  })
+  .catch((err) => console.error('Server not running 😥, cause ' + err));
