9088e1af02c02705cd47a9356fc0df28ce4e01f9
felipsbreno <brendo.filipe2050@gmail.com>
2023-06-04 20:26:56 -0300

    add cors in sever project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import { fastify } from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 const app = fastify()
 
 const portServer: number = 3333
 
+app.register(cors, { origin: true })
 app.register(memoriesRoutes)
-
 app
   .listen({
     port: portServer,
