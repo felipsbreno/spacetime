bc8a62def5eaa772230a4a61b7ce18a10f18544f
felipsbreno <brendo.filipe2050@gmail.com>
2023-06-04 13:18:18 -0300

    remove prettier extension from vscode to apply configure of eslint it rocketseat

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
-import { fastify } from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { fastify } from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-const portServer: number = 3333;
-const app = fastify();
-const prisma = new PrismaClient();
+const portServer: number = 3333
+const app = fastify()
+const prisma = new PrismaClient()
 
 app.get('/users', async () => {
-  const users = await prisma.user.findMany();
-  return users;
-});
+  const users = await prisma.user.findMany()
+  return users
+})
 
 app
   .listen({
@@ -16,7 +16,7 @@ app
   })
   .then(() => {
     console.log(
-      '🚀 HTTP server running on port ' + `http://localhost:${portServer}`
-    );
+      '🚀 HTTP server running on port ' + `http://localhost:${portServer}`,
+    )
   })
-  .catch((err) => console.error('Server not running 😥, cause' + err));
+  .catch((err) => console.error('Server not running 😥, cause' + err))
