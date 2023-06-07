132eb942281ab19128c9380737459dd6846fc2e8
felipsbreno <brendo.filipe2050@gmail.com>
2023-06-07 18:43:45 -0300

    create route to make upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,27 @@ import 'dotenv/config'
 import { fastify } from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
-const app = fastify()
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
+const app = fastify()
 const portServer: number = 3333
 
 app.register(cors, { origin: true })
 app.register(jwt, { secret: 'spacetime' })
+app.register(multipart)
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app
   .listen({
     port: portServer,
